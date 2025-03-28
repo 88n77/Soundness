899701a79b686c5658fc7134c31cfbc99dbddc77
Nazar8 <118745073+88n77@users.noreply.github.com>
2025-03-28 18:47:24 +0000

    8

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt update && apt install -y git
+
+if ! command -v rustc &> /dev/null; then
+            curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+            source $HOME/.cargo/env
+        else
+            rustup update
+            source $HOME/.cargo/env
+        fi
+
+source $HOME/.cargo/env
+
+curl -sSL https://raw.githubusercontent.com/soundnesslabs/soundness-layer/main/soundnessup/install | bash
+source ~/.bashrc
